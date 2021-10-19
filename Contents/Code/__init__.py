eaa9a9ec4d1e3531f7546baf847c2c7659f8b518
Sacha Weatherstone <sachaw100@hotmail.com>
2021-10-19 16:51:40 +1100

    Change cache time & allow local media metadata

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,18 +5,20 @@ EN = Locale.Language.English
 
 
 def ValidatePrefs():
-    #maybe do something here?
+    # maybe do something here?
     return MessageContainer('Success', "DefaultPrefs.json valid")
 
+
 def Start():
-    HTTP.CacheTime = CACHE_1DAY
+    HTTP.CacheTime = CACHE_1MINUTE*30
+
 
 class AniListTV(Agent.TV_Shows):
     name = 'AniList'
-    languages = [ EN ]
+    languages = [EN]
     primary_provider = True
     fallback_agent = False
-    accepts_from = None
+    accepts_from = ['com.plexapp.agents.localmedia']
     contributes_to = None
 
     def search(self, results, media, lang, manual):
@@ -25,12 +27,13 @@ class AniListTV(Agent.TV_Shows):
     def update(self, metadata, media, lang, force):
         update_anime('tv', metadata, media, force)
 
+
 class AniListMovie(Agent.Movies):
     name = 'AniList'
-    languages = [ EN ]
+    languages = [EN]
     primary_Provider = True
     fallback_agent = False
-    accepts_from = None
+    accepts_from = ['com.plexapp.agents.localmedia']
     contributes_to = None
 
     def search(self, results, media, lang, manual):
